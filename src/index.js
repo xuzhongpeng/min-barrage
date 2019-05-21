9c6de150a1f0ec8ab80d971245d7ce3766ce21a6
xuzhongpeng <1452754335@qq.com>
2019-05-21 11:51:05 +0800

    [add]配置打包

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Barrage from '../lib/bundle'
-import './css/style.scss'
+import Barrage from '../lib/barrage'
+import '../lib/barrage.css'
 function component() {
     let barrage=new Barrage({
       info:'a',
@@ -9,13 +9,9 @@ function component() {
     });
     let i=1
     //let a=setInterval(()=>{
-      barrage.start('我的大哥是拱格式发顺丰'+i++,{speed:10,headImg:'static/1.jpeg'});
-      barrage.start('我的大哥是拱格式发顺丰'+i++,{speed:10,url:'http://www.baidu.com'});
-      barrage.start();
-      barrage.start('gogog')
-    //   if(i>100){
-    //     i=1
-    //   }
-    // },1000)
+    barrage.start('我的大哥是拱格式发顺丰'+i++,{speed:10,headImg:'static/1.jpeg'});
+    barrage.start('我的大哥是拱格式发顺丰'+i++,{speed:10,url:'http://www.baidu.com'});
+    barrage.start();
+    barrage.start('gogog')
   } 
   component()
---
+++ webpack.config.pro.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+module.exports = {
+  mode:'production',
+  entry: {
+    //app:path.resolve(__dirname,'src/index.js'),
+    barrage:'./src/js/barrage.ts'
+  },
+  output: {
+    filename: 'barrage.js',
+    path: path.resolve(__dirname, 'lib'),
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, './tsconfig.json')
+            },
+          },
+          'astroturf/loader'
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    })
+  ],
+};
